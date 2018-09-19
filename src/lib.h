e41f505f7de9f83e05c705d39c03299c450762e8
bougoumasarah <s.bougouma@epsi.fr>
2018-09-19 11:42:29 +0200

    initiale

---
+++ src/lib.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class calcul 
+{
+public :
+int somme (int nombre_1 ,int nombre_2);
+};
+
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdlib.h>
+#include<cstdio>
+int main()
+{
+int a;
+int b;
+
+std::cout<<"Choissisez la valeur de a:"<<std::endl;
+std::cin>>a;
+std::cout<<"choissisz la valeur de b"<<std::endl;
+std::cin>>b;
+std::cout<<"resulta du calcule= "<<a+b<<std::endl;
+return 0;
+}
